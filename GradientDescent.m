6b8511720e9958145bc95661d298a321962ede62
hopkinslaurel <hopkins.laurel@gmail.com>
2016-10-08 15:20:04 -0700

    Update GradientDescent.m

--- GradientDescent.m
+++ GradientDescent.m
@@ -1,20 +1,38 @@
 % Initializations
-upper = 10;
+upper = 10000;
 train = load('C:\Users\Laurel\Matlab\CS534\src\hw1\train.csv');
 test = load('C:\Users\Laurel\Matlab\CS534\src\hw1\test.csv');
 numfeatures = size(test,2)-1;
 weights = zeros(upper, numfeatures);
 costHistory = zeros(upper, upper);
+converged = 0;
 
-for k = 1:upper
-    learningRate = 0.1*k;
-    [weights(k,:), costHistory(k,:)] = gradientDescent(learningRate, train, test);
+%for k = 1:upper
+ %   learningRate = 0.1*k;
+ %  [weights(k,:), costHistory(k,:)] = gradientDescent(learningRate, train, test);  
+%end
+
+k = 1;
+while(~converged)
     
-    figure;
-    plot(1:upper, costHistory(k,:), '-b', 'LineWidth', 2);
+   [weights(k,:), costHistory(k,:)] = gradientDescent(learningRate, train, test);
+   
+   if k > 1
+        cost_previous = sum(costHistory(k-1,:));
+        cost_current = sum(costHistory(k,:));
+    
+      if (cost_previous - cost_current)/cost_previous < 0.001
+            converged = 1;
+      end
+   end
+ 
+   k = k + 1;
+end
+
+ figure;
+    plot(1:upper, costHistory(1,:), '-b', 'LineWidth', 2);
     xlabel('Number of iterations');
     ylabel('Cost');
-end
 
 figure;
 plot(1:numel(J_history), J_history, '-b', 'LineWidth', 2);
@@ -60,11 +78,12 @@ end
 
         %j is result of  the loss function J(w)
         j = ((x * weights) - y)'; %currently spits back y-transpose b/c weights just set x to zero
-
+        gradientJ = 2*j* x;
+        
         for k = 1:lenWts
 
             %store recalculated weight into weight cache
-            weightCache(k) = weights(k) - learningRate * (1/lenY) * j * x(:, k);
+            weightCache(k) = weights(k) - learningRate * (1/lenY) * gradientJ(:,k)';
 
             %update the weights
             weights(k) = weightCache(k);
@@ -79,7 +98,7 @@ end
 
     function [SSE] = cost(x, y, weights)
         %costVal = (x * weights - y)' * (x * weights - y) / (2 * (length(y)));
-        SS = sum((x * weights - y)' * (x * weights - y));
-        SSE = SS/(2*(length(y)));
+        SS = sum((y - x * weights)' * (y - x * weights));
+        SSE = SS/((length(y)));
     end
     
