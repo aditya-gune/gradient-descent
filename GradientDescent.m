8bf2ad5eac1a6c83a57a59652bcd7bc2542e138e
agune001 <ca_adi@yahoo.com>
2016-10-08 16:04:20 -0700

    Revert "Merge pull request #3 from hopkinslaurel/patch-4"
    This reverts commit 351b6c8e9683e1e898febbe0c6cc651bcaf91d1d, reversing
    changes made to 88f271e07470910e5222bef08c7a915d1ec3ba1a.
    
    Reverted merge of gradientDescent.m

--- GradientDescent.m
+++ GradientDescent.m
@@ -1,38 +1,20 @@
 % Initializations
-upper = 10000;
+upper = 10;
 train = load('C:\Users\Laurel\Matlab\CS534\src\hw1\train.csv');
 test = load('C:\Users\Laurel\Matlab\CS534\src\hw1\test.csv');
 numfeatures = size(test,2)-1;
 weights = zeros(upper, numfeatures);
 costHistory = zeros(upper, upper);
-converged = 0;
 
-%for k = 1:upper
- %   learningRate = 0.1*k;
- %  [weights(k,:), costHistory(k,:)] = gradientDescent(learningRate, train, test);  
-%end
-
-k = 1;
-while(~converged)
+for k = 1:upper
+    learningRate = 0.1*k;
+    [weights(k,:), costHistory(k,:)] = gradientDescent(learningRate, train, test);
     
-   [weights(k,:), costHistory(k,:)] = gradientDescent(learningRate, train, test);
-   
-   if k > 1
-        cost_previous = sum(costHistory(k-1,:));
-        cost_current = sum(costHistory(k,:));
-    
-      if (cost_previous - cost_current)/cost_previous < 0.001
-            converged = 1;
-      end
-   end
- 
-   k = k + 1;
-end
-
- figure;
-    plot(1:upper, costHistory(1,:), '-b', 'LineWidth', 2);
+    figure;
+    plot(1:upper, costHistory(k,:), '-b', 'LineWidth', 2);
     xlabel('Number of iterations');
     ylabel('Cost');
+end
 
 figure;
 plot(1:numel(J_history), J_history, '-b', 'LineWidth', 2);
@@ -78,12 +60,11 @@ end
 
         %j is result of  the loss function J(w)
         j = ((x * weights) - y)'; %currently spits back y-transpose b/c weights just set x to zero
-        gradientJ = 2*j* x;
-        
+
         for k = 1:lenWts
 
             %store recalculated weight into weight cache
-            weightCache(k) = weights(k) - learningRate * (1/lenY) * gradientJ(:,k)';
+            weightCache(k) = weights(k) - learningRate * (1/lenY) * j * x(:, k);
 
             %update the weights
             weights(k) = weightCache(k);
@@ -98,7 +79,7 @@ end
 
     function [SSE] = cost(x, y, weights)
         %costVal = (x * weights - y)' * (x * weights - y) / (2 * (length(y)));
-        SS = sum((y - x * weights)' * (y - x * weights));
-        SSE = SS/((length(y)));
+        SS = sum((x * weights - y)' * (x * weights - y));
+        SSE = SS/(2*(length(y)));
     end
     
